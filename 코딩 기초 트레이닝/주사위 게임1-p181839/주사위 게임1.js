143f2410b1b17d9720d51a1a44701abd92bea007
kitez <duswl339@ajou.ac.kr>
2023-07-31 17:17:17 +0900

    주사위 게임1

---
+++ 코딩 기초 트레이닝/주사위 게임1-p181839/주사위 게임1.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181839
+
+function solution(a, b) {
+    // a and b odd -> a^2 + b^2
+    // a or b odd -> 2 * (a + b)
+    // a and b even -> | a - b |
+    if( a % 2 !== 0 && b % 2 !== 0 ) {
+        return a**2 + b **2;
+    } else if( a % 2 === 0 && b % 2 ===0) {
+        return Math.abs(a-b);
+    } else {
+        return 2 * (a+b);
+    }
+}
