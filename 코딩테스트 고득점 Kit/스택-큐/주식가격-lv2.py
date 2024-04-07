2b6f9abee6fb500b1079d92e661dcde44c0f542d
Kite_Z <duswl339@ajou.ac.kr>
2024-04-07 16:12:01 +0900

    solve 스택/큐-주식가격

---
+++ 코딩테스트 고득점 Kit/스택-큐/주식가격-lv2.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+def solution(prices):
+    answer = []
+
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i, len(prices) - 1):
+            if prices[i] <= prices[j]:
+                cnt += 1
+            else:
+                break
+        answer.append(cnt)
+
+    return answer
